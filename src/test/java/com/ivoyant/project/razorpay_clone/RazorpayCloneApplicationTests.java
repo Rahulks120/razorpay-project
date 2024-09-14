3402f6e8981658d80166d922b84732eb2eaaa3fb
Rahulks120 <rahulk@ivoyant.com>
2024-09-14 19:32:54 +0530

    first commit

---
+++ src/test/java/com/ivoyant/project/razorpay_clone/RazorpayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoyant.project.razorpay_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
